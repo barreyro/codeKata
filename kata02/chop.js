09cdc663cd580a1434fe482ba8322da06598e26f
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-10-01 15:16:44 -0400

    add chop solution

--- kata02/chop.js
+++ kata02/chop.js
@@ -1,3 +1,30 @@
 var chop = function(value, array) {
+  if (array.length === 0){
+    return -1;
+  } else {
+    return findIndex(value, array);
+  }
 
+ function findIndex(val, arr, count){
+   var length = arr.length;
+   var midpoint = parseInt(length/2);
+   var indexCount = count || 0;
+
+   if (val> arr[length - 1]){
+     return -1;
+   } else if (val < arr[0]){
+     return -1;
+   } else if(val == arr[midpoint]) {
+     return midpoint + indexCount;
+   } else if(val == arr[0]){
+     return indexCount;
+   } else if (val < arr[midpoint]){
+     return findIndex(val, arr.splice(0, (midpoint-1)));
+   } else if (val > arr[midpoint]){
+     return findIndex(val, arr.splice(midpoint, length), midpoint);
+   } else {
+     console.log('error');
+   }
+ }
 };
+
